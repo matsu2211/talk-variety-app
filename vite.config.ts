e9603f39aea2b1d4558c3641eccbf75cbfebc786
matsu2211 <hanaxmsc@gmail.com>
2026-03-02 21:48:14 +0900

    お題追加・編集

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: "/talk-variety-app/",
+      
       server: {
         port: 3000,
-        host: '0.0.0.0',
+        host: '0.0.0.0'
       },
       plugins: [react()],
       define: {
